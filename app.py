3da9a2c620d65de90f56a02397be31ac4bb43d8f
Gridnev Ilja <mr.gridnev@gmail.com>
2022-06-12 21:48:58 +0300

    some local testing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import os
 import urllib.request
 
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +12,10 @@ app = Flask(__name__)
 
 @app.get('/')
 def home():
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode()
+    # For local testing IP is local host, so external api is used
+    # external_ip = urllib.request.urlopen('https://ident.me').read().decode()
+    external_ip = request.remote_addr
+
     api_url = f"http://api.ipstack.com/{external_ip}?access_key={API_KEY}&fields=latitude,longitude"
     response = urllib.request.urlopen(api_url).read().decode()
     longitude, latitude = json.loads(response).values()
